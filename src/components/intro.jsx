35ff6f163100b04eeabca7ce3292e5a79178803d
BernardoDeveloper <bernardogualberto8@gmail.com>
2023-04-09 11:41:05 -0300

    feat: automatic calculate month date

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,15 +1,22 @@
-import { Bold } from '../handlers/bold';
 import { ButtonLink } from '../handlers/buttonLink';
 
 function Intro() {
+  const months = () => {
+    const loveDate = new Date('September 24, 2022');
+    const now = new Date();
+
+    return loveDate.getMonth() + 1 - now.getMonth();
+  };
+
   return (
     <section className="flex flex-col justify-between items-center space-y-10">
       <div className="space-y-3">
         <h2 className="w-full text-2xl font-bold text-start">Eternamente:</h2>
 
         <p className="font-karla text-black/80">
-          Aynoã, você é o amor da minha vida. Eu te agradeço por tudo, não
-          existe palavras que consiga descrever nosso amor.
+          {months()} meses perfeitos e maravilhosos. Aynoã, você é o amor da
+          minha vida. Eu te agradeço por tudo, não existe palavras que consiga
+          descrever nosso amor.
         </p>
       </div>
 
